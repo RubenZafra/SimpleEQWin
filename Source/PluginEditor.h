982b9bd54728763bf815d148155029bac61a49f8
Ruben Zafra <rubenreamps@gmail.com>
2021-10-02 16:58:27 +0200

    Added Basic Slider Visuals

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,12 +13,12 @@
 
 struct LookAndFeel : juce::LookAndFeel_V4
 {
-    void drawRotarySlider (juce::Graphics&,
-                           int x, int y, int width, int height,
-                           float sliderPosProportional,
-                           float rotaryStartAngle,
-                           float rotaryEndAngle,
-                           juce::Slider&) override { }
+    void drawRotarySlider(juce::Graphics&,
+        int x, int y, int width, int height,
+        float sliderPosProportional,
+        float rotaryStartAngle,
+        float rotaryEndAngle,
+        juce::Slider&) override;
 };
 
 
