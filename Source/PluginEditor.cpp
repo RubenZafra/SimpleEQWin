b01eb898ccf8f871f482f484e7940f210c6138ea
Ruben Zafra <rubenreamps@gmail.com>
2021-10-02 18:29:39 +0200

    Fix Errors 2

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -56,13 +56,7 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
 
         g.setColour(Colours::white);
         g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
-
     }
-
-
-
-
-
 }
 
 //=================================================================================
@@ -76,11 +70,7 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
 
     auto range = getRange();
 
-        
-
     auto sliderBounds = getSliderBounds();
-
-
 
     //g.setColour(Colours::red);
     //g.drawRect(getLocalBounds());
@@ -121,7 +111,6 @@ void RotarySliderWithLabels::paint(juce::Graphics& g)
         r.setY(r.getY() + getTextHeight());
 
         g.drawFittedText(str, r.toNearestInt(), juce::Justification::centred, 1);
-
     }
 }
 
@@ -140,7 +129,6 @@ juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
     r.setY(2);
 
     return r;
-
 
 }
 
@@ -177,7 +165,6 @@ juce::String RotarySliderWithLabels::getDisplayString() const
 
         str << suffix;
     }
-
     return str;
 }
 
@@ -232,79 +219,76 @@ void ResponseCurveComponent::updateChain()
     updateCutFilter(monoChain.get<ChainPositions::LowCut>(), lowCutCoefficients, chainSettings.lowCutSlope);
     updateCutFilter(monoChain.get<ChainPositions::HighCut>(), highCutCoefficients, chainSettings.highCutSlope);
 }
-
-void ResponseCurveComponent::paint(juce::Graphics& g)
-{
-    using namespace juce;
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll(Colours::black);
-
-    auto responseArea = getLocalBounds();
-
-
-
-    auto w = responseArea.getWidth();
-    auto& lowcut = monoChain.get<ChainPositions::LowCut>();
-    auto& peak = monoChain.get<ChainPositions::Peak>();
-    auto& highcut = monoChain.get<ChainPositions::HighCut>();
-
-    auto sampleRate = audioProcessor.getSampleRate();
-
-    std::vector<double> mags;
-
-    mags.resize(w);
-
-    for (int i = 0; i < w; ++i)
-    {
-        double mag = 1.f;
-        auto freq = mapToLog10(double(i) / double(w), 20.0, 20000.0);
-
-        if (!monoChain.isBypassed<ChainPositions::Peak>())
-            mag *= peak.coefficients->getMagnitudeForFrequency(freq, sampleRate);
-        if (!lowcut.isBypassed<0>())
-            mag *= lowcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
-        if (!lowcut.isBypassed<1>())
-            mag *= lowcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
-        if (!lowcut.isBypassed<2>())
-            mag *= lowcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
-        if (!lowcut.isBypassed<3>())
-            mag *= lowcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
-
-        if (!highcut.isBypassed<0>())
-            mag *= highcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
-        if (!highcut.isBypassed<1>())
-            mag *= highcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
-        if (!highcut.isBypassed<2>())
-            mag *= highcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
-        if (!highcut.isBypassed<3>())
-            mag *= highcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
-
-        mags[i] = Decibels::gainToDecibels(mag);
-
-    }
-
-    Path responseCurve;
-
-    const double outputMin = responseArea.getBottom();
-    const double outputMax = responseArea.getY();
-    auto map = [outputMin, outputMax](double input)
-    {
-        return jmap(input, -24.0, 24.0, outputMin, outputMax);
-    };
-
-    responseCurve.startNewSubPath(responseArea.getX(), map(mags.front()));
-
-    for (size_t i = 1; i < mags.size(); ++i)
-    {
-        responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
-    }
-
-    g.setColour(Colours::orange);
-    g.drawRoundedRectangle(responseArea.toFloat(), 4.f, 1.f);
-
-    g.setColour(Colours::white);
-    g.strokePath(responseCurve, PathStrokeType(2.f));
-};
+//
+//void ResponseCurveComponent::paint(juce::Graphics& g)
+//{
+//    using namespace juce;
+//    // (Our component is opaque, so we must completely fill the background with a solid colour)
+//    g.fillAll(Colours::black);
+//
+//    auto responseArea = getLocalBounds();
+//
+//    auto w = responseArea.getWidth();
+//    auto& lowcut = monoChain.get<ChainPositions::LowCut>();
+//    auto& peak = monoChain.get<ChainPositions::Peak>();
+//    auto& highcut = monoChain.get<ChainPositions::HighCut>();
+//
+//    auto sampleRate = audioProcessor.getSampleRate();
+//
+//    std::vector<double> mags;
+//
+//    mags.resize(w);
+//
+//    for (int i = 0; i < w; ++i)
+//    {
+//        double mag = 1.f;
+//        auto freq = mapToLog10(double(i) / double(w), 20.0, 20000.0);
+//
+//        if (!monoChain.isBypassed<ChainPositions::Peak>())
+//            mag *= peak.coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//        if (!lowcut.isBypassed<0>())
+//            mag *= lowcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//        if (!lowcut.isBypassed<1>())
+//            mag *= lowcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//        if (!lowcut.isBypassed<2>())
+//            mag *= lowcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//        if (!lowcut.isBypassed<3>())
+//            mag *= lowcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//
+//        if (!highcut.isBypassed<0>())
+//            mag *= highcut.get<0>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//        if (!highcut.isBypassed<1>())
+//            mag *= highcut.get<1>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//        if (!highcut.isBypassed<2>())
+//            mag *= highcut.get<2>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//        if (!highcut.isBypassed<3>())
+//            mag *= highcut.get<3>().coefficients->getMagnitudeForFrequency(freq, sampleRate);
+//
+//        mags[i] = Decibels::gainToDecibels(mag);
+//    }
+//
+//    Path responseCurve;
+//
+//    const double outputMin = responseArea.getBottom();
+//    const double outputMax = responseArea.getY();
+//    auto map = [outputMin, outputMax](double input)
+//    {
+//        return jmap(input, -24.0, 24.0, outputMin, outputMax);
+//    };
+//
+//    responseCurve.startNewSubPath(responseArea.getX(), map(mags.front()));
+//
+//    for (size_t i = 1; i < mags.size(); ++i)
+//    {
+//        responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
+//    }
+//
+//    g.setColour(Colours::orange);
+//    g.drawRoundedRectangle(responseArea.toFloat(), 4.f, 1.f);
+//
+//    g.setColour(Colours::white);
+//    g.strokePath(responseCurve, PathStrokeType(2.f));
+//};
 
 
 //==============================================================================
@@ -366,12 +350,11 @@ SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 }
 
 //==============================================================================
-void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
+void SimpleEQAudioProcessorEditor::paint(juce::Graphics& g)
 {
     using namespace juce;
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll(Colours::black);
-
 } 
 
 void SimpleEQAudioProcessorEditor::resized()
